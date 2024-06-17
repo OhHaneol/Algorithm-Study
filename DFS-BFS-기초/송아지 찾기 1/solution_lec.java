d9a05d75944bf244bb84e37bd53f49259e553c74
Haneol Oh <otyvs1109@hs.ac.kr>
2024-06-17 12:09:13 +0900

    Create solution_lec.java

---
+++ DFS-BFS-기초/송아지 찾기 1/solution_lec.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    int[] dis = {1, -1, 5};
+    int[] ch;   // 직선상 좌표점 들린 곳 체크
+    Queue<Integer> Q = new LinkedList<>();
+
+    public int missingCow(int s, int e) {
+        ch = new int[10001];
+        ch[s] = 1;  // 출발 지점 체크
+        Q.offer(s);
+        int L = 0;
+        while (!Q.isEmpty()) {
+            int len = Q.size();
+            for (int i = 0; i < len; i++) {
+                int x = Q.poll();
+                if (x == e) return L;
+                for (int j = 0; j < 3; j++) {
+                    int nx = x + dis[j];
+                    if (nx >= 1 && nx <= 10000 && ch[nx] == 0) {
+                        ch[nx] = 1;
+                        Q.offer(nx);
+                    }
+                }
+            }
+            L++;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int S = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+
+        System.out.println(T.missingCow(S, E));
+    }
+}
